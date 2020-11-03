6d57d9c6145ad4e8fccc19ccea98cc82e2bb5dbe
Nick Polychronakis <nickpolychronakis@me.com>
2020-11-03 22:09:24 +0200

    Έτοιμο για χρήση.

--- Sources/DateExtensions/DateExtensions.swift
+++ Sources/DateExtensions/DateExtensions.swift
@@ -1,3 +1,74 @@
-struct DateExtensions {
-    var text = "Hello, World!"
+import Foundation
+
+extension Date {
+    // MARK: - Format Date
+    /// Μετατρέπει την ημερομήνία και την ώρα σε String σε πλήρη μορφή ( αν θέλουμε μπορούμε να προσθέσουμε επιπλέον λεπτά)
+    public func longDateAndTimeToString(_ addedMinutes: Double = 0.0) -> String {
+        let date = Date(timeInterval: addedMinutes * 60, since: self)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        
+        var tempDateAndTime = ""
+        // Αν η γλώσσα της εφαρμογής είναι Ελληνικά
+        if Locale.preferredLanguages[0] == "el" {
+            // κανονικά γίνεται με το .short, αλλά στα ελληνικά αντί να γράψει "στις" (όπως στα αγγλικά βάζει "at", βάζει παύλα, επομένως έπρεπε να το αλλάξω μόνος μου
+            dateFormatter.timeStyle = .none // .short
+            tempDateAndTime = "\(dateFormatter.string(from: date)) και ώρα \(date.timeToString())"
+        // Αν είναι οποιαδήποτε άλλη γλώσσα
+        } else {
+            dateFormatter.timeStyle = .short
+            tempDateAndTime = "\(dateFormatter.string(from: date))"
+        }
+        return tempDateAndTime
+    }
+    
+    /// Μετατρέπει την ημερομήνία και την ώρα σε String ( αν θέλουμε μπορούμε να προσθέσουμε επιπλέον λεπτά)
+    public func shortDateAndTimeToString(_ addedMinutes: Double = 0.0) -> String {
+        let date = Date(timeInterval: addedMinutes * 60, since: self)
+        
+        var tempDateAndTime = ""
+        // Αν η γλώσσα της εφαρμογής είναι Ελληνικά
+        if Locale.preferredLanguages[0] == "el" {
+            tempDateAndTime = "\(date.dateToString()) και ώρα \(date.timeToString())"
+        // Αν είναι οποιαδήποτε άλλη γλώσσα θα μπαίνει μπαίνει "," μεταξύ της ημερομηνίας και της ώρας
+        } else {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .short
+            dateFormatter.timeStyle = .short
+            tempDateAndTime = dateFormatter.string(from: date)
+        }
+        return tempDateAndTime
+    }
+    
+    /// Μετατρέπει μόνο την ημερομήνία σε String ( αν θέλουμε μπορούμε να προσθέσουμε επιπλέον ημέρες)
+    public func dateToString(_ addedDays: Double = 0.0) -> String {
+        let date = Date(timeInterval: addedDays * 60 * 60 * 24, since: self)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        return dateFormatter.string(from: date)
+    }
+    
+    /// Επιστρέφει μόνο την ώρα ( αν θέλουμε μπορούμε να προσθέσουμε επιπλέον λεπτά)
+    public func timeToString(_ addedMinutes: Double = 0.0) -> String {
+        let date = Date(timeInterval: addedMinutes * 60, since: self)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .none
+        dateFormatter.timeStyle = .short
+        return dateFormatter.string(from: date)
+    }
+    
+    /// Επιστρέφει το στοιχείο που επιθυμούμε απο μία ημερομηνία σε String
+    public func dateComponentToString(component: Calendar.Component) -> String {
+        let dateComponent = Calendar.current.component(component, from: self)
+        
+        switch component {
+        case .month:
+            return DateFormatter().monthSymbols[dateComponent - 1]
+        case .weekday:
+            return DateFormatter().weekdaySymbols[dateComponent - 1]
+        default:
+            return String(dateComponent)
+        }
+    }
 }
--- Tests/DateExtensionsTests/DateExtensionsTests.swift
+++ Tests/DateExtensionsTests/DateExtensionsTests.swift
@@ -3,13 +3,91 @@ import XCTest
 
 final class DateExtensionsTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DateExtensions().text, "Hello, World!")
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(DateExtensions().text, "Hello, World!")
     }
 
     static var allTests = [
         ("testExample", testExample),
     ]
+}
+
+// ΜΕΓΑΛΗ ΗΜΕΡΟΜΗΝΙΑ ΚΑΙ ΩΡΑ
+final class LongDateAndTimeToStringTests: XCTestCase {
+    // Ελέγχω αν δουλεύει σωστά η συνάρτηση
+    func testLongDateAndTimeToString() {
+        let tempDate = Date(timeIntervalSinceReferenceDate: 0).longDateAndTimeToString()
+        let correctResult = "Monday, January 1, 2001 at 2:00 AM"
+        XCTAssertEqual(tempDate, correctResult)
+    }
+    // Ελέγχω αν δουλεύει σωστά η συνάρτηση με προσθήκη χρόνου
+    func testLongDateAndTimeToStringWithAddedTime() {
+        // Προσθέτω πέντε ώρες για να είμαι σίγουρος ότι δουλεύει σωστά η προσθήκη ώρας
+        let tempDate = Date(timeIntervalSinceReferenceDate: 60 * 60 * 5).longDateAndTimeToString()
+        let correctResult = "Monday, January 1, 2001 at 7:00 AM"
+        XCTAssertEqual(tempDate, correctResult)
+    }
+}
+
+// ΜΙΚΡΗ ΗΜΕΡΟΜΗΝΙΑ ΚΑΙ ΩΡΑ
+final class ShortDateAndTimeToStringTests: XCTestCase {
+    // Ελέγχω αν δουλεύει σωστά η συνάρτηση
+    func testShortDateAndTimeToString() {
+        let tempDate = Date(timeIntervalSinceReferenceDate: 0).shortDateAndTimeToString()
+        let correctResult = "1/1/01, 2:00 AM"
+        XCTAssertEqual(tempDate, correctResult)
+    }
+    // Ελέγχω αν δουλεύει σωστά η συνάρτηση με προσθήκη χρόνου
+    func testShortDateAndTimeToStringWithAddedTime() {
+        // Προσθέτω πέντε ώρες για να είμαι σίγουρος ότι δουλεύει σωστά η προσθήκη ώρας
+        let tempDate = Date(timeIntervalSinceReferenceDate: 60 * 60 * 5).shortDateAndTimeToString()
+        let correctResult = "1/1/01, 7:00 AM"
+        XCTAssertEqual(tempDate, correctResult)
+    }
+}
+
+// ΗΜΕΡΟΜΗΝΙΑ
+final class DateToStringTests: XCTestCase {
+    // Ελέγχει την συνάρτηση
+    func testDateToString() {
+        let tempDate = Date(timeIntervalSinceReferenceDate: 0).dateToString()
+        let correctResult = "1/1/01"
+        XCTAssertEqual(tempDate, correctResult)
+    }
+    // Ελέγχω αν δουλεύει σωστά η συνάρτηση με προσθήκη ημερών
+    func testDateToStringWithAddedTime() {
+        // Προσθέτω πέντε ώρες για να είμαι σίγουρος ότι δουλεύει σωστά η προσθήκη ώρας
+        let tempDate = Date(timeIntervalSinceReferenceDate: 0).dateToString(5)
+        let correctResult = "1/6/01"
+        XCTAssertEqual(tempDate, correctResult)
+    }
+}
+
+// ΩΡΑ
+final class TimeToStringTests: XCTestCase {
+    // Ελέγχει την συνάρτηση
+    func testTimeToString() {
+        let tempDate = Date(timeIntervalSinceReferenceDate: 0).timeToString()
+        let correctResult = "2:00 AM"
+        XCTAssertEqual(tempDate, correctResult)
+    }
+    // Ελέγχω αν δουλεύει σωστά η συνάρτηση με προσθήκη χρόνου
+    func testTimeToStringWithAddedTime() {
+        // Προσθέτω πέντε ώρες για να είμαι σίγουρος ότι δουλεύει σωστά η προσθήκη ώρας
+        let tempDate = Date(timeIntervalSinceReferenceDate: 60 * 60 * 5).timeToString()
+        let correctResult = "7:00 AM"
+        XCTAssertEqual(tempDate, correctResult)
+    }
+}
+
+// ΣΤΟΙΧΕΙΟ ΗΜΕΡΟΜΗΝΙΑΣ
+final class DateComponentTests: XCTestCase {
+    // Ελέγχει την συνάρτηση
+    func testDateComponent() {
+        let tempDate = Date(timeIntervalSinceReferenceDate: 0).dateComponentToString(component: .weekday)
+        let correctResult = "Monday"
+        XCTAssertEqual(tempDate, correctResult)
+    }
 }
